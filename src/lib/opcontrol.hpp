0e450b4cc5bd513cb7e45f665e9a5f362ae7dcef
Davis Liu <davisliu2006@gmail.com>
2023-08-08 15:18:59 -0400

    Update opcontrol.hpp

--- src/lib/opcontrol.hpp
+++ src/lib/opcontrol.hpp
@@ -7,9 +7,9 @@ inline void opcontrol_start() {
     is_opcontrol = true;
 
     while (is_opcontrol) {
-        double x = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-        double y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        double rot = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        double x = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X)/127.0;
+        double y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y)/127.0;
+        double rot = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)/127.0;
         flmotor.move_velocity((y+rot)*GRN_RPM);
         frmotor.move_velocity((y-rot)*GRN_RPM);
         rlmotor.move_velocity((y+rot)*GRN_RPM);
